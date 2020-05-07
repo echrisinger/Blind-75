23b1a89dae8cc60f0bf764e1705c1bd0bcdd20b2
Evan Chrisinger <echrisinger@gmail.com>
2020-05-06 20:07:15 -0700

    .

---
+++ eraseOverlapIntervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # sort by end, descending, start descending.
+        intervals = sorted(intervals, key=lambda i: (i[1], -i[0]))
+        max_interval_end = -1 * float('inf')
+        erased = 0
+        for i in intervals:
+            if i[0] < max_interval_end:
+                erased += 1
+            else:
+                max_interval_end = i[1]
+                
+        return erased
+    
