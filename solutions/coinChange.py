84ed005c932d788ce15420f41a8c61a5f79bfcbf
Evan Chrisinger <echrisinger@gmail.com>
2020-05-17 22:33:01 -0700

    coin change

--- solutions/coinChange.py
+++ solutions/coinChange.py
@@ -1,15 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coin_counts = [0] * (amount+1)
-            
-        for curr_amt in range(1, amount+1):
-            sub_val = float('inf')
-            
-            for c in coins:
-                sub_amount = curr_amt - c
-                if sub_amount >= 0:
-                    sub_val = min(sub_val, coin_counts[sub_amount])
-                        
-                coin_counts[curr_amt] = sub_val + 1
+        coins.sort()
+        table = [-1]*(amount+1)
+        table[0] = 0
         
-        return coin_counts[amount] if coin_counts[amount] != float('inf') else -1
+        for curr_amt in range(1, amount+1):            
+            subtotal_no = float('inf')
+            for c in coins:
+                coin_subtotal = curr_amt - c
+                if coin_subtotal < 0:
+                    break
+                if table[coin_subtotal] >= 0:
+                    subtotal_no = min(subtotal_no, table[coin_subtotal])
+            
+            if subtotal_no != float('inf'):
+                table[curr_amt] = subtotal_no + 1
+        
+        return table[amount]
+            
